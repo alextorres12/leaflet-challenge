08f933d952353ce3fef44fbe6d112691698133b0
Alex Torres <62618236+alextorres12@users.noreply.github.com>
2021-02-18 18:53:30 -0500

    adds working map with base layers

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,14 +1,16 @@
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson"
 
+// Get request to queryUrl
 d3.json(queryUrl, function(data){
    createFeatures(data.features);
-})
+});
 
 function createFeatures(earthquakeData) {
     // Function to bind a popup to each feature in the features array
     function onEachFeature(feature, layer) {
         layer.bindPopup("<h3>" + feature.properties.place +
         "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+        
     }
 
     // Create GeoJSON layer containing the features array
@@ -19,4 +21,52 @@ function createFeatures(earthquakeData) {
 
     // Pass earthquake layer to map creation function
     createMap(earthquakes);
+}
+
+function createMap(earthquakes){
+    
+    // Define streetmap layer
+    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+    });
+
+    // Define darkmap layer
+    var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "dark-v10",
+        accessToken: API_KEY
+    });
+
+    // Define baseMaps object to hold base layers
+    var baseMaps = {
+        "Street Map": streetmap,
+        "Dark Map": darkmap
+    };
+
+    // Create overlay object to hold overlay layer
+    var overlayMaps = {
+        Earthquakes: earthquakes
+    };
+
+    // Create map with streetmap and earthquakes to display on load
+    var myMap = L.map("mapid", {
+        center: [9.304964883671476, -79.42759905850454],
+        zoom: 5,
+        layers: [streetmap, earthquakes]
+    });
+
+    /*
+    * Create layer control,
+    * pass in baseMaps and overlayMaps,
+    * add layer control to the map
+    */
+   L.control.layers(baseMaps, overlayMaps, {
+       collapsed: false
+   }).addTo(myMap);
 }
