c24c3ca827729161586f5f8ea955c8423b9aa865
Alex Torres <62618236+alextorres12@users.noreply.github.com>
2021-02-18 18:35:52 -0500

    create features function

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,9 +1,22 @@
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson"
 
 d3.json(queryUrl, function(data){
-   console.log(data.features);
+   createFeatures(data.features);
 })
 
-function createFeatures(earthquakedata) {
-    
+function createFeatures(earthquakeData) {
+    // Function to bind a popup to each feature in the features array
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+
+    // Create GeoJSON layer containing the features array
+    // Run the function once for each piece of data in the array
+    var earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature
+    });
+
+    // Pass earthquake layer to map creation function
+    createMap(earthquakes);
 }
